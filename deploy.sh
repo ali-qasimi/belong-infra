5c7d9c5d60c6ea70897beccfd871faaef9a93c4a
Ali <43767565+ali-qasimi@users.noreply.github.com>
2023-11-24 18:50:41 +1100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@
 #to execute on a CI/CD platform.
 #requirements: kubectl, terraform cli, docker
 
+cd terraform
 terraform init
 terraform plan
 terraform apply
 
+cd ..
 docker image build . --tag belong-app:latest
 
 kubectl apply -f ./kubernetes/deployment.yaml
